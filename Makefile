78ee99d28681e01e5915a54b1745fd61d0261608
Anasstasiya Sviridenkova <anastasia_sviridenkova@mail.ru>
2025-08-27 18:17:47 +0500

    init project

---
+++ Makefile
@@ -0,0 +1,14 @@
+run-dist:
+	make -C app run-dist
+
+build:
+	make -C app build
+
+checkstyle:
+	make -C app checkstyle
+
+test:
+	make -C app test
+
+report:
+	make -C app report
---
+++ app/src/test/java/hexlet/code/EmptyTest.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EmptyTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
